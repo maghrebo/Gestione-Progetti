e6f68e8313ba1309b516e1f3bc57eb1699a21033
maghrebo <raouf.amine@iisgalvanimi.edu.it>
2024-11-14 08:44:36 +0100

    metodo aggiunta elementi

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 
 lista = ['banane', 'biscotti', 'caramelle', 'merendine', 'nutella biscuits', 'crema sunsilk']
+
+def aggiunta():
+
+    while True:
+
+        scelta = input('Vuoi aggiungere elementi alla lista? (digita si o no):   ')
+        if scelta == 'si':
+            aggiunta = input('Che elemento della spesa vorresti aggiungere:  ')
+            lista.append(aggiunta)
+            print(lista)
+        elif scelta == 'no':
+            print('Va bene hai scelto di no, grazie e arrivederci')
+            break
+        else:
+            print('Hai sbagliato l\'inserimento della scelta, riprova: ')
+
