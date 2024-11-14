e515ab24515bd92306ce3965af4f1edcc58bf7b8
maghrebo <raouf.amine@iisgalvanimi.edu.it>
2024-11-14 09:01:24 +0100

    metodo rimozione

--- main.py
+++ main.py
@@ -17,3 +17,25 @@ def aggiunta():
 
 def stampa_elementi():
     print(lista)
+
+def eliminazione():
+
+    while True:
+        decisione = input('Vuoi eliminare un elemento (digita si o no):  ')
+        if decisione == 'si':
+            while True:
+                try:
+                    scelta = int(input('Quale elemento vorresti eliminare (inserisci indice):  '))
+                    lista.pop(scelta)
+                    break
+                except:
+                    print('Hai inserito un carattere diverso da un numero oppure l\'indice non è presente nella lista, riprova')
+                    break
+                
+                    
+        elif decisione == 'no':
+            print('Va bene sarai rimandato al menu principale, grazie ')
+            break
+        else:
+            print('Hai inserito una scelta diversa da quelle possibili, riprova')
+
