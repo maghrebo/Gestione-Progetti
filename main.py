408a404494c9788c3e8699cabecba2a6cc23ae59
maghrebo <raouf.amine@iisgalvanimi.edu.it>
2024-11-14 09:32:49 +0100

    fase 2 finita

--- main.py
+++ main.py
@@ -39,3 +39,28 @@ def eliminazione():
         else:
             print('Hai inserito una scelta diversa da quelle possibili, riprova')
 
+def decisione(scelta):
+    if scelta == 1:
+        aggiunta()
+    elif scelta == 2:
+        stampa_elementi()
+    elif scelta == 3:
+        eliminazione()
+        
+
+def menu_utente():
+    while True:
+        print(' Selezionare un opzione: \n 1 Aggiungi elementi alla lista \n 2 Visualizza gli elementi della lista \n 3 Eliminare un elemento \n 4 Chiudere il programma')
+        scelta = int(input('Inserisci la tua scelta:  '))
+
+        if scelta == 4:
+            print("Va bene il programma verra chiuso, grazie e arrivederci ")
+            break
+        elif scelta == 1 or scelta == 2 or scelta == 3:
+            decisione(scelta)
+        else:
+            print(  "Hai inserito un carattere diverso da un numero oppure maggiore dalle scelte disponibili, riprova.")
+            
+
+menu_utente()
+        
