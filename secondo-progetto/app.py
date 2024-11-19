ff2e627dcff71a450225daf3cb94f80f64f2054f
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-11-19 08:26:53 +0000

    secondo progetto - controllo lista

--- secondo-progetto/app.py
+++ secondo-progetto/app.py
@@ -1,13 +1,23 @@
 from flask import Flask
 
+#Dichiarazione della lista spesa
+lista_spesa = []
+
 #inizializza l'app Flask
 app = Flask(__name__)
 
 #rotta principale
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
-    \
+    
+    
+    #Controllo della lista se è piena o meno e le seguenti azioni
+    if len(lista_spesa) == 0 :
+        return "La lista è vuota" 
+    else:
+        return(lista_spesa)
+
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
+
