90e45bdfe7d2c3543c80b5818798a8e14df40688
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-11-19 08:47:16 +0000

    secondo progetto - meta form

--- secondo-progetto/app.py
+++ secondo-progetto/app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
+
+#request per fare delle richieste sia GET che POST da un client
+#redirect per reindirazzare l'utente a un altra pagina o link
+#url_for per la creazione di un url dinamico e non statico e per aver meno errori
+
 
 #Dichiarazione della lista spesa
 lista_spesa = []
@@ -10,12 +15,24 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     
+    return render_template('index.html')
     
     #Controllo della lista se è piena o meno e le seguenti azioni
     if len(lista_spesa) == 0 :
         return "La lista è vuota" 
     else:
         return(lista_spesa)
+
+@app.route('/aggiungi', methods=['POST'])
+#Creazione della funzione per l'aggiunta di un elemento alla lista
+def aggiungi(elemento):
+    #Si estrae la variabile dalla pagina
+    elemento = request.form['elemento']
+    #Si fa l'if per aggiungerlo
+    if elemento:
+        lista_spesa.append(elemento)
+    #Reindirizzamento alla pagina principale
+    return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
